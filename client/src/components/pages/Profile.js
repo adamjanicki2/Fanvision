03c5a14818d909aec8a79fcf3a36de3a1c4e88cc
henryw8 <76844174+henryw8@users.noreply.github.com>
2021-01-15 14:45:25 -0600

    profile page updates
    added name and award count

--- client/src/components/pages/Profile.js
+++ client/src/components/pages/Profile.js
@@ -2,25 +2,37 @@ import React, { Component } from "react";
 
 import "../../utilities.css";
 import "./Profile.css";
-
+import { get, post } from "../../utilities.js";
 
 
 class Profile extends Component {
   constructor(props) {
     super(props);
     // Initialize Default State
-    this.state = {};
+    this.state = {
+      name: "",
+      gold_count: 0,
+      silver_count: 0,
+      bronze_count: 0,
+    };
   }
 
   componentDidMount() {
     // remember -- api calls go here!
-  }
+    get("/api/whoami").then((user) => {
+      console.log(user.name);
+      this.setState({name: user.name})
+    })
+  };
 
   render() {
     return (
       <>
-        <h1>Temp Profile Page</h1>
-        <h2>Hello World</h2>
+        <h1>{this.state.name}</h1>
+        <h2>This is your profile page!</h2>
+        <h2>Gold: {this.state.gold_count}</h2>
+        <h2>Silver: {this.state.silver_count}</h2>
+        <h2>Bronze: {this.state.bronze_count}</h2>
       </>
     );
   }

92a10699616e4a7d4a8e6876c4665ba503a857c7
Adam Janicki <adamjanicki2@gmail.com>
2021-01-16 17:28:48 -0500

    pp4

--- client/src/components/modules/NextGameCard.js
+++ client/src/components/modules/NextGameCard.js
@@ -5,11 +5,6 @@ import "./NextGameCard.css";
 class NextGameCard extends Component {
     constructor(props) {
       super(props);
-      this.state = { //hard coded for now
-          home_team: "DAL",
-          away_team: "PHI",
-          time: "18:00 ET",
-      };
     }
 
     componentDidMount(){
@@ -20,8 +15,8 @@ class NextGameCard extends Component {
         return(
             <>
                 <div className="NextGameCard-container">
-                    <div>{this.state.away_team} vs {this.state.home_team}</div>
-                    <div className="NextGameCard-time">{this.state.time}</div>
+                    <div>{this.props.away_team} vs {this.props.home_team}</div>
+                    <div className="NextGameCard-time">{this.props.time}</div>
                 </div> 
             </>
         );

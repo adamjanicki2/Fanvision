d5bf34ae28fddb903daa4841961e61504992f67a
henryw8 <76844174+henryw8@users.noreply.github.com>
2021-01-18 14:42:28 -0600

    result card style
    added winner by margin for easier comparison to your prediction

--- client/src/components/modules/ResultGameCard.js
+++ client/src/components/modules/ResultGameCard.js
@@ -10,15 +10,32 @@ class ResultGameCard extends Component {
 
     }
 
+    getWinner (){
+        const home_score = this.props.home_team_score;
+        const away_score = this.props.away_team_score;
+        if (home_score>away_score){
+            return this.props.home_team;
+        } else if (home_score<away_score){
+            return this.props.away_team;
+        } else {
+            return "TIE"
+        }}
+
+    getMargin () {
+        return Math.abs(this.props.home_team_score-this.props.away_team_score);
+    }
+
     render() {
         return(
             <>
                 <div className="ResultGameCard-container">
                     <div>{this.props.away_team} vs {this.props.home_team}</div>
-                    <div className="ResultGameCard-scoreIdentifier">Actual Result:</div>
+                    <div className="ResultGameCard-scoreIdentifier">Final Score:</div>
                     <div className="ResultGameCard-scoreActual">{this.props.away_team_score} - {this.props.home_team_score}</div>
                     <div className="ResultGameCard-scoreIdentifier">Your Prediction:</div>
                     <div className="ResultGameCard-scorePredicted">{this.props.predicted_winner} by {this.props.predicted_margin}</div>
+                    <div className="ResultGameCard-scoreIdentifier">Actual Result:</div>
+                    <div className="ResultGameCard-scorePredicted">{this.getWinner()} by {this.getMargin()}</div>
                     <div className="ResultGameCard-scoreIdentifier">Points Earned:</div>
                     <div>{this.props.points_earned}</div>
                 </div> 

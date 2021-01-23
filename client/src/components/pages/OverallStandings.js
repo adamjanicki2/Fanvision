63e98cc51b1a3a28c71342f5b40d75259491c805
henryw8 <76844174+henryw8@users.noreply.github.com>
2021-01-22 20:04:26 -0600

    table revamp

--- client/src/components/pages/OverallStandings.js
+++ client/src/components/pages/OverallStandings.js
@@ -13,19 +13,52 @@ class OverallStandings extends Component {
     this.state = {
       user_id: null,
       scoreboard: [],
+      time:'',
+      name:null,
     };
   }
 
   componentDidMount() {
     get("/api/whoami").then((user) => {
-      this.setState({user_id: user._id,})
+      this.setState({user_id: user._id, name:user.name})
     });
 
     get('/api/getscoreboard').then((board) => {
       this.setState({scoreboard: board});
     });
 
+    get('/api/current_time').then((res) => {
+      this.setState({time:res.time});
+    });
+
   }
+
+  dateConvert = (inDate)=> {
+    //takes in string "yyyy-mm-dd" and returns string in proper english"
+    const date_list = inDate.split("-");
+    let year = date_list[0];
+    let month = date_list[1];
+    let day = date_list[2];
+    const MONTH_CONVERSION={
+        "01":"January",
+        "02":"February",
+        "03":"March",
+        "04":"April",
+        "05":"May",
+        "06":"June",
+        "07":"July",
+        "08":"August",
+        "09":"September",
+        "10":"October",
+        "11":"November",
+        "12":"December",
+    }
+    if (day[0]==="0"){
+        day=day[1]
+    }
+    return MONTH_CONVERSION[month]+" "+day+", "+year
+}
+
 
 
   render() {
@@ -46,25 +79,31 @@ class OverallStandings extends Component {
         </motion.div>
         </div>
       </div>
-
-    <table>
-        <tbody>
-        <tr className='table-header'>
-          <th>Rank</th>
-          <th>Player Name</th>
-          <th>Total Points</th>
-          <th>Medals</th>
+    <h1 className="updatedText">Updated as of {this.dateConvert(this.state.time.substring(0,10))} @ {this.state.time.slice(-5)} ET</h1>
+    <div className="table-container">
+    <table className='styled-table'>
+       <thead >
+        <tr>
+          <th className='tableheadertext'>Rank</th>
+          <th className='tableheadertext'>Player Name</th>
+          <th className='tableheadertext'>Total Points</th>
+          <th className='tableheadertext'>Medals</th>
         </tr>
+        </thead>
+    <tbody>
     {this.state.scoreboard.map((entry, i) => 
-      <tr className='table-entry'>
-        <td>{i+1}</td>
-        <td>{entry.name}</td>
-        <td>{entry.current_score}</td>
-        <td>{entry.gold} Gold, {entry.silver} Silver, {entry.bronze} Bronze</td>
+      <tr>
+        
+        {entry.name === this.state.name ? <td className = 'extra-bold your-row'>{i+1}</td> : <td className = 'extra-bold'>{i+1}</td>}
+        {entry.name === this.state.name ? <td className='your-row'>{entry.name}</td> : <td>{entry.name}</td>}
+        {entry.name === this.state.name ? <td className = 'your-row'>{entry.current_score}</td> : <td>{entry.current_score}</td>}
+        {entry.name === this.state.name ? <td className = 'your-row'>{entry.gold} Gold, {entry.silver} Silver, {entry.bronze} Bronze</td> : <td>{entry.gold} Gold, {entry.silver} Silver, {entry.bronze} Bronze</td>}
+
       </tr>
     )}
     </tbody>
     </table>
+    </div>
     </>) : (<div className="center-screen"><Loader type="Grid" color="black" height={50} width={50}/></div>);
 
     let html_to_return = this.state.user_id ? score_table : (<h2>Please log in and refresh to display Standings!</h2>);

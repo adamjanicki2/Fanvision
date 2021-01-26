353a7ed4924decb1dde7ca911e6104c7b934150e
Adam Janicki <adamjanicki2@gmail.com>
2021-01-26 17:15:23 -0500

    profile background covers page
    profile background covers page

--- client/src/components/pages/NotFound.js
+++ client/src/components/pages/NotFound.js
@@ -9,7 +9,7 @@ class NotFound extends Component {
     return (
         <div className="NotFound-header">
           <header className="NotFound-headerText">404 Not Found</header>
-          <h3 className="NotFound-headersubText">The requested page could not be found.</h3>
+          <h3 className="NotFound-headersubText">Oops! The requested page "{window.location.pathname}" could not be found.</h3>
         </div>
       
     );

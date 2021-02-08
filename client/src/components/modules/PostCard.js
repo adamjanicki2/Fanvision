c232c34364464011497be895eb73c7ec2b2b8cc0
henryw8 <76844174+henryw8@users.noreply.github.com>
2021-02-08 17:29:59 -0600

    PostCard creared

---
+++ client/src/components/modules/PostCard.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import "./PostCard.css";
+import { convertMilitary, dateConvert } from '../../utilities.js';
+
+class PostCard extends Component {
+    constructor(props) {
+      super(props);
+
+
+      
+    }
+
+    componentDidMount(){
+        
+      }
+
+      
+
+    render() {
+       return (
+       <div className='PostCard-container'>
+           <div className='PostCard-username'>{this.props.user}</div>
+           <div className='PostCard-message'>I cannot believe this. As a lifelong NBA fan it’s disappointing to see LeFraud get a Mickey Mouse ring this year. Le3-6 needs to go cry to the refs to win game and get carried by AD. LeBum isn’t top 25 all time. Steph curry owns LeTrash in the Finals. Absolute Joke. Not my Goat.</div>
+        </div>)
+        
+  }}         
+    export default PostCard;
--- client/src/components/pages/Forum.js
+++ client/src/components/pages/Forum.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import '../../utilities.css';
 import "./Forum.css";
+import PostCard from "../modules/PostCard.js";
 import { get, post } from "../../utilities.js";
 
 class Forum extends Component {
@@ -20,7 +21,9 @@ class Forum extends Component {
     return (
         <div>
           <h1 className='u-textCenter'>Forum!</h1>
+          <PostCard user='Kevin LeBryant'></PostCard>
         </div>
+        
       
     );
   }
